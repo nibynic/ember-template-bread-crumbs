0c948392f943dddd788c05fa4e7834c872c31aec
Piotrek Bator <piotrek@nibynic.com>
2020-10-21 20:15:43 +0200

    Fixed updating breadcrumb text

--- addon/helpers/bread-crumb.js
+++ addon/helpers/bread-crumb.js
@@ -1,6 +1,7 @@
 import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
 import { assign } from '@ember/polyfills';
+import { setProperties } from '@ember/object';
 
 export default class BreadCrumbHelper extends Helper {
   @service('bread-crumbs') breadCrumbs;
@@ -8,7 +9,7 @@ export default class BreadCrumbHelper extends Helper {
   compute([text], hash) {
     let data = assign({ text }, hash);
     if (this.registration) {
-      assign(this.registration, data);
+      setProperties(this.registration, data);
     } else {
       this.registration = this.breadCrumbs.register(data);
     }
