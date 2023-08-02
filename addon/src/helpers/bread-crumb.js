9374d2b622aced174d882aead596b96e3b9bc85b
Paweł <720354+jembezmamy@users.noreply.github.com>
2023-08-02 12:59:05 +0200

    compatibility

--- addon/src/helpers/bread-crumb.js
+++ addon/src/helpers/bread-crumb.js
@@ -1,13 +1,12 @@
 import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
-import { assign } from '@ember/polyfills';
 import { setProperties } from '@ember/object';
 
 export default class BreadCrumbHelper extends Helper {
   @service('bread-crumbs') breadCrumbs;
 
   compute([text], hash) {
-    let data = assign({ text }, hash);
+    let data = Object.assign({ text }, hash);
     if (this.registration) {
       setProperties(this.registration, data);
     } else {
